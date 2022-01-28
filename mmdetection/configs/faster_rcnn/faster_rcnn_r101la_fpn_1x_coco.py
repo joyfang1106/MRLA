5f0a833aa1896effb13d1a98fb611edb6be3d665
Joy Fang <35486045+fangyanwen1106@users.noreply.github.com>
2022-01-28 14:38:23 +0800

    Add files via upload

---
+++ mmdetection/configs/faster_rcnn/faster_rcnn_r101la_fpn_1x_coco.py
@@ -0,0 +1,7 @@
+_base_ = './faster_rcnn_r50la_fpn_1x_coco.py'
+model = dict(
+    backbone=dict(
+        # depth=101,
+        layers=[3, 4, 23, 3], 
+        init_cfg=dict(type='Pretrained',
+                      checkpoint='./pretrained/r101_rla_78.4.pth.tar')))
