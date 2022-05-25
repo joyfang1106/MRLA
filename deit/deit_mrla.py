e67f19547b752852267c95617244065329ce34a6
Joy Fang <35486045+fangyanwen1106@users.noreply.github.com>
2022-05-25 20:31:14 +0800

    Add files via upload

--- deit/deit_mrla.py
+++ deit/deit_mrla.py
@@ -39,52 +39,8 @@ Wv: conv2d-gelu
 
 __all__ = [
     'deit_mrla_tiny_patch16_224', 'deit_mrla_small_patch16_224', 'deit_mrla_base_patch16_224',
-    # 'deit_tiny_distilled_patch16_224', 'deit_small_distilled_patch16_224',
-    # 'deit_base_distilled_patch16_224', 'deit_base_patch16_384',
-    # 'deit_base_distilled_patch16_384',
 ]
 
-
-# class DistilledViT_RLA(ViT_RLA):
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.dist_token = nn.Parameter(torch.zeros(1, 1, self.embed_dim))
-#         num_patches = self.patch_embed.num_patches
-#         self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 2, self.embed_dim))
-#         self.head_dist = nn.Linear(self.embed_dim, self.num_classes) if self.num_classes > 0 else nn.Identity()
-
-#         trunc_normal_(self.dist_token, std=.02)
-#         trunc_normal_(self.pos_embed, std=.02)
-#         self.head_dist.apply(self._init_weights)
-
-#     def forward_features(self, x):
-#         # taken from https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/vision_transformer.py
-#         # with slight modifications to add the dist_token
-#         B = x.shape[0]
-#         x = self.patch_embed(x)
-
-#         cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
-#         dist_token = self.dist_token.expand(B, -1, -1)
-#         x = torch.cat((cls_tokens, dist_token, x), dim=1)
-
-#         x = x + self.pos_embed
-#         x = self.pos_drop(x)
-
-#         for blk in self.blocks:
-#             x = blk(x)
-
-#         x = self.norm(x)
-#         return x[:, 0], x[:, 1]
-
-#     def forward(self, x):
-#         x, x_dist = self.forward_features(x)
-#         x = self.head(x)
-#         x_dist = self.head_dist(x_dist)
-#         if self.training:
-#             return x, x_dist
-#         else:
-#             # during inference, return the average of both classifier predictions
-#             return (x + x_dist) / 2
 
 
 class PatchEmbed(nn.Module):
@@ -476,6 +432,48 @@ def _init_vit_weights(module: nn.Module, name: str = '', head_bias: float = 0., 
         nn.init.ones_(module.weight)
         
         
+# class DistilledViT_mrla(ViT_mrla):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.dist_token = nn.Parameter(torch.zeros(1, 1, self.embed_dim))
+#         num_patches = self.patch_embed.num_patches
+#         self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 2, self.embed_dim))
+#         self.head_dist = nn.Linear(self.embed_dim, self.num_classes) if self.num_classes > 0 else nn.Identity()
+
+#         trunc_normal_(self.dist_token, std=.02)
+#         trunc_normal_(self.pos_embed, std=.02)
+#         self.head_dist.apply(self._init_weights)
+
+#     def forward_features(self, x):
+#         # taken from https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/vision_transformer.py
+#         # with slight modifications to add the dist_token
+#         B = x.shape[0]
+#         x = self.patch_embed(x)
+
+#         cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
+#         dist_token = self.dist_token.expand(B, -1, -1)
+#         x = torch.cat((cls_tokens, dist_token, x), dim=1)
+
+#         x = x + self.pos_embed
+#         x = self.pos_drop(x)
+
+#         for blk in self.blocks:
+#             x = blk(x)
+
+#         x = self.norm(x)
+#         return x[:, 0], x[:, 1]
+
+#     def forward(self, x):
+#         x, x_dist = self.forward_features(x)
+#         x = self.head(x)
+#         x_dist = self.head_dist(x_dist)
+#         if self.training:
+#             return x, x_dist
+#         else:
+#             # during inference, return the average of both classifier predictions
+#             return (x + x_dist) / 2
+
+
 
 @register_model
 def deit_mrla_tiny_patch16_224(pretrained=False, **kwargs):
