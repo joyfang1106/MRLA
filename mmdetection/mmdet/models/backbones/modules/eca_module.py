c238ee8b63a7947581a7172f8c50c75f7cc5162b
Joy Fang <35486045+joyfang1106@users.noreply.github.com>
2023-03-02 18:59:03 +0800

    Add files via upload

---
+++ mmdetection/mmdet/models/backbones/modules/eca_module.py
@@ -0,0 +1,36 @@
+import torch
+from torch import nn
+from torch.nn.parameter import Parameter
+from math import log
+
+
+# from .modules.eca_module import eca_layer
+class eca_layer(nn.Module):
+    """Constructs a ECA module.
+    Args:
+        channel: Number of channels of the input feature map
+        k_size: Adaptive selection of kernel size
+        source: https://github.com/BangguWu/ECANet
+    """
+    def __init__(self, channel, k_size=None):
+        super(eca_layer, self).__init__()
+        if k_size == None:
+            t = int(abs((log(channel, 2) + 1) / 2.))
+            k_size = t if t % 2 else t+1
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False) 
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # x: input features with shape [b, c, h, w]
+        b, c, h, w = x.size()
+        # feature descriptor on the global spatial information
+        y = self.avg_pool(x)
+
+        # obtain attention weights
+        y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+        y = self.sigmoid(y)
+
+        return x * y.expand_as(x)
+    
+    
