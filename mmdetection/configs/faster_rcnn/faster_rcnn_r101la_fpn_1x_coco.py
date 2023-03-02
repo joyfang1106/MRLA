b983184f9356c034bde7d69f1adb25a98d65f5b6
Joy Fang <35486045+joyfang1106@users.noreply.github.com>
2023-03-02 17:55:21 +0800

    Delete faster_rcnn_r101la_fpn_1x_coco.py

--- mmdetection/configs/faster_rcnn/faster_rcnn_r101la_fpn_1x_coco.py
+++
@@ -1,7 +0,0 @@
-_base_ = './faster_rcnn_r50la_fpn_1x_coco.py'
-model = dict(
-    backbone=dict(
-        # depth=101,
-        layers=[3, 4, 23, 3], 
-        init_cfg=dict(type='Pretrained',
-                      checkpoint='./pretrained/r101_mrla_78.66.pth.tar')))
